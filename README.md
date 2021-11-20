5bc44d68bad5f3a076f8f8e55ddebe3e8bc60314
Jorge <jorge.reich@me.com>
2021-11-20 17:12:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SwiftUIForms
---
+++ SwiftUIForms/SwiftUIFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormsApp.swift
+//  SwiftUIForms
+//
+//  Created by Jorge-Alberto Reich on 20.11.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
